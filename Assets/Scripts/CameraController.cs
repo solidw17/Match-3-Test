4e66d302fb570969c1085981ce6b6105a067f045
GustavoHRM <gustavohrm17@gmail.com>
2020-07-29 22:23:20 -0300

    Aspect Ratio fixes

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,7 +7,7 @@ public class CameraController : MonoBehaviour
     // Start is called before the first frame update
     void Awake()
     {
-        float orthoSize = 6 * Screen.height / Screen.width * 0.5f;
+        float orthoSize = 7f * Screen.height / Screen.width * 0.5f;
 
         Camera.main.orthographicSize = orthoSize;
     }
